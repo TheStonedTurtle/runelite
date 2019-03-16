5932cb07ce6fdbc7c536f53ece573fa863625174
TheStonedTurtle <29030969+TheStonedTurtle@users.noreply.github.com>
2019-03-15 23:47:11 -0700

    Fix config

--- runelite-client/src/main/java/net/runelite/client/plugins/performancetracker/PerformanceTrackerConfig.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/performancetracker/PerformanceTrackerConfig.java
@@ -24,11 +24,12 @@
  */
 package net.runelite.client.plugins.performancetracker;
 
+import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
 
 @ConfigGroup("performancetracker")
-public interface PerformanceTrackerConfig
+public interface PerformanceTrackerConfig extends Config
 {
 	@ConfigItem(
 		position = 0,
