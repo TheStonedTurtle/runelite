25731dead9913231aa7dfff742f6d442fcc46efb
TheStonedTurtle <29030969+TheStonedTurtle@users.noreply.github.com>
2019-03-15 23:07:55 -0700

    Update NpcExpModifier.java

--- runelite-client/src/main/java/net/runelite/client/plugins/performancetracker/NpcExpModifier.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/performancetracker/NpcExpModifier.java
@@ -30,33 +30,35 @@ import lombok.Getter;
 import net.runelite.api.NpcID;
 
 /**
- * The Experience received from damaging an NPC is actually based on the NPCs stats.
- * For stronger NPCs this can cause you to gain more XP for the same damage dealt.
+ * The Experience received from damaging an NPC is based on the NPCs stats. This causes certain NPCs to have
+ * an exp modifier which can be calculated by this class.
  */
 @Getter
 enum NpcExpModifier
 {
-	// TODO: Confirm all NPC ids, some are probably incorrect or just useless
-	// Common
+	// TODO: Add missing NPCs and remove unused ids
 	DAGANNOTH_REX(255, 255, 255, 255, 255, 255, 255, 0, 0, NpcID.DAGANNOTH_REX),
 	DAGANNOTH_PRIME(255, 255, 255, 255, 255, 255, 255, 0, 0, NpcID.DAGANNOTH_PRIME),
 	DAGANNOTH_SUPREME(225, 255, 128, 255, 10, 10, 10, 0, 0, NpcID.DAGANNOTH_SUPREME),
 	GIANT_MOLE(200, 200, 200, 200, 60, 80, 100, 0, 0, NpcID.GIANT_MOLE),
 	KQ(300, 300, 300, 255, 50, 50, 10, 0, 0, NpcID.KALPHITE_QUEEN, NpcID.KALPHITE_QUEEN_963, NpcID.KALPHITE_QUEEN_965),
 	KQ_2(300, 300, 300, 255, 100, 100, 100, 0, 0, NpcID.KALPHITE_QUEEN_4303, NpcID.KALPHITE_QUEEN_4304),
-	// Other
 	VORKATH(560, 308, 214, 750, 26, 108, 108, 16, 0, NpcID.VORKATH, NpcID.VORKATH_8058, NpcID.VORKATH_8059, NpcID.VORKATH_8060, NpcID.VORKATH_8061),
+
 	// All Zulrah phase stats are the same for the values we care about
 	ZULRAH(1, 1, 300, 500, 0, 0, 0, 0, 0, NpcID.ZULRAH, NpcID.ZULRAH_2043, NpcID.ZULRAH_2044),
+
 	// Sporadic
 	OBOR(90, 100, 60, 120, 35, 40, 45, 100, 68, NpcID.OBOR),
 	BRYOPHYTA(130, 100, 100, 115, 0, 0, 0, 33, 31, NpcID.BRYOPHYTA),
 	SKOTIZO(240, 250, 200, 450, 80, 80, 80, 160, 31, NpcID.SKOTIZO),
+
 	// GWD
 	KREEARRA(300, 200, 260, 255, 180, 180, 180, 136, 12, NpcID.KREEARRA),
 	GENERAL_GRAARDOR(280, 350, 250, 255, 90, 90, 90, 120, 43, NpcID.GENERAL_GRAARDOR),
 	COMMANDER_ZILYANA(280, 196, 300, 255, 100, 100, 100, 195, 20, NpcID.COMMANDER_ZILYANA),
 	KRIL_TSUTSAROTH(340, 300, 270, 255, 80, 80, 80, 160, 31, NpcID.KRIL_TSUTSAROTH),
+
 	// Slayer
 	DUSK(200, 1400, 100, 450, 0, 0, 0, 0, 0, NpcID.DUSK, NpcID.DUSK_7851, NpcID.DUSK_7854, NpcID.DUSK_7855,
 		NpcID.DUSK_7882, NpcID.DUSK_7883, NpcID.DUSK_7886, NpcID.DUSK_7887, NpcID.DUSK_7888, NpcID.DUSK_7889),
@@ -66,6 +68,7 @@ enum NpcExpModifier
 	KRAKEN(1, 1, 1, 255, 0, 0, 0, 0, 0, NpcID.KRAKEN, NpcID.KRAKEN_6640, NpcID.KRAKEN_6656),
 	CERBERUS(220, 220, 100, 600, 50, 100, 25, 50, 0, NpcID.CERBERUS, NpcID.CERBERUS_5863, NpcID.CERBERUS_5866),
 	SMOKE_DEVIL(240, 220, 360, 240, 11, 4, 9, 0, 0, NpcID.THERMONUCLEAR_SMOKE_DEVIL),
+
 	// Theatre of Blood
 	MAIDEN(350, 350, 200, 3500, 0, 0, 0, 0, 0,
 		NpcID.THE_MAIDEN_OF_SUGADINTI, NpcID.THE_MAIDEN_OF_SUGADINTI_8361, NpcID.THE_MAIDEN_OF_SUGADINTI_8362,
@@ -122,7 +125,7 @@ enum NpcExpModifier
 	 * Based off the formula found here: http://services.runescape.com/m=forum/c=PLuJ4cy6gtA/forums.ws?317,318,712,65587452,209,337584542#209
 	 * @return bonus XP modifier
 	 */
-	private double calculateXpModifier()
+	public double calculateXpModifier()
 	{
 		final double averageLevel = Math.floor((attack + strength + defence + hp) / 4);
 		final double averageDefBonus = Math.floor((stab + slash + crush) / 3);
