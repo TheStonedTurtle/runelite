8b0c96fd8d7ed531cf720dafb149b7cf6c48c52a
TheStonedTurtle <mgoodwin559@gmail.com>
2019-10-17 16:50:21 -0700

    Fix StackFormatter and AsyncBufferedImage imports

--- runelite-client/src/main/java/net/runelite/client/plugins/stonedtracker/ui/SelectionPanel.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/stonedtracker/ui/SelectionPanel.java
@@ -45,7 +45,6 @@ import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
-import net.runelite.client.game.AsyncBufferedImage;
 import net.runelite.client.game.ItemManager;
 import net.runelite.client.plugins.stonedtracker.data.BossTab;
 import net.runelite.client.ui.ColorScheme;
@@ -53,6 +52,7 @@ import net.runelite.client.ui.PluginPanel;
 import net.runelite.client.ui.components.IconTextField;
 import net.runelite.client.ui.components.materialtabs.MaterialTab;
 import net.runelite.client.ui.components.materialtabs.MaterialTabGroup;
+import net.runelite.client.util.AsyncBufferedImage;
 import org.apache.commons.text.similarity.JaroWinklerDistance;
 
 public class SelectionPanel extends JPanel
@@ -274,7 +274,7 @@ public class SelectionPanel extends JPanel
 				materialTab.setVerticalAlignment(SwingConstants.CENTER);
 				materialTab.setPreferredSize(new Dimension(35, 35));
 			};
-			image.onChanged(resize);
+			image.onLoaded(resize);
 			resize.run();
 
 			thisTabGroup.addTab(materialTab);
