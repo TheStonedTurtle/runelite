6f66b6c6fff8226e8f916e709edfe2f4dc6fccb9
TheStonedTurtle <mgoodwin559@gmail.com>
2020-02-02 03:03:50 -0800

    Update notifier to account for CustomNotifier annotation

--- runelite-client/src/main/java/net/runelite/client/Notifier.java
+++ runelite-client/src/main/java/net/runelite/client/Notifier.java
@@ -62,6 +62,7 @@ import net.runelite.client.chat.ChatColorType;
 import net.runelite.client.chat.ChatMessageBuilder;
 import net.runelite.client.chat.ChatMessageManager;
 import net.runelite.client.chat.QueuedMessage;
+import net.runelite.client.config.CustomNotifier;
 import net.runelite.client.config.FlashNotification;
 import net.runelite.client.config.RuneLiteConfig;
 import net.runelite.client.ui.ClientUI;
@@ -111,6 +112,7 @@ public class Notifier
 	private final boolean terminalNotifierAvailable;
 	private Instant flashStart;
 	private long mouseLastPressedMillis;
+	private FlashNotification flashNotification;
 
 	@Inject
 	private Notifier(
@@ -132,32 +134,42 @@ public class Notifier
 			!Strings.isNullOrEmpty(RuneLiteProperties.getLauncherVersion())
 			&& isTerminalNotifierAvailable();
 
+		flashNotification = runeliteConfig.flashNotification();
+
 		storeIcon();
 	}
 
 	public void notify(String message)
 	{
-		notify(message, TrayIcon.MessageType.NONE);
+		notify(message, TrayIcon.MessageType.NONE, null);
 	}
 
 	public void notify(String message, TrayIcon.MessageType type)
 	{
-		if (!runeLiteConfig.sendNotificationsWhenFocused() && clientUI.isFocused())
+		notify(message, type, null);
+	}
+
+	public void notify(String message, TrayIcon.MessageType type, CustomNotifier custom)
+	{
+		final boolean useCustom = custom != null && runeLiteConfig.enableCustomNotifier() && custom.enableCustomNotifier();
+		final CustomNotifier config = useCustom ? custom : ((CustomNotifier) runeLiteConfig);
+
+		if (!config.sendNotificationsWhenFocused() && clientUI.isFocused())
 		{
 			return;
 		}
 
-		if (runeLiteConfig.requestFocusOnNotification())
+		if (config.requestFocusOnNotification())
 		{
 			clientUI.requestFocus();
 		}
 
-		if (runeLiteConfig.enableTrayNotifications())
+		if (config.enableTrayNotifications())
 		{
 			sendNotification(appName, message, type);
 		}
 
-		switch (runeLiteConfig.notificationSound())
+		switch (config.notificationSound())
 		{
 			case NATIVE:
 				Toolkit.getDefaultToolkit().beep();
@@ -166,7 +178,7 @@ public class Notifier
 				executorService.submit(this::playCustomSound);
 		}
 
-		if (runeLiteConfig.enableGameMessageNotification() && client.getGameState() == GameState.LOGGED_IN)
+		if (config.enableGameMessageNotification() && client.getGameState() == GameState.LOGGED_IN)
 		{
 			final String formattedMessage = new ChatMessageBuilder()
 				.append(ChatColorType.HIGHLIGHT)
@@ -180,7 +192,8 @@ public class Notifier
 				.build());
 		}
 
-		if (runeLiteConfig.flashNotification() != FlashNotification.DISABLED)
+		flashNotification = config.flashNotification();
+		if (flashNotification != FlashNotification.DISABLED)
 		{
 			flashStart = Instant.now();
 			mouseLastPressedMillis = client.getMouseLastPressedMillis();
@@ -196,8 +209,6 @@ public class Notifier
 			flashStart = null;
 			return;
 		}
-
-		FlashNotification flashNotification = runeLiteConfig.flashNotification();
 
 		if (client.getGameCycle() % 40 >= 20
 			// For solid colour, fall through every time.
